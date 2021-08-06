cc19fff3360814fc86830022982a5e8dd4125903
Keith Francis Balde <keithfrancisb@gmail.com>
2021-08-05 22:45:35 -0400

    delete user data source

--- server/datasources/index.js
+++ server/datasources/index.js
@@ -1,9 +1,5 @@
 const GoogleBooksAPI = require('./GoogleBooksAPI');
-const UserDataSource = require('./Users');
-
-const User = require('../models/User');
 
 module.exports = () => ({
   googleBooksAPI: new GoogleBooksAPI(),
-  userDataSource: new UserDataSource(User)
 });
